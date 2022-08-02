185192833b89bd3cc97b482104c0bc10098b2dd3
Arturo Canler <arturocanler@gmail.com>
2022-08-01 23:57:22 -0400

    Added store and api services for friends feature

--- src/app/components/app-component/app.component.spec.ts
+++ src/app/components/app-component/app.component.spec.ts
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-        'AngularDemo1 app is running!'
+      'AngularDemo1 app is running!'
     );
   });
 });
