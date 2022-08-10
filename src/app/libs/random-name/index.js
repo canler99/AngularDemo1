5349e434c848aeb422fd4d789a9a2bce0840f4bf
Arturo Canler <arturocanler@gmail.com>
2022-08-09 23:26:33 -0400

    Updated screenshot to include in readme file. Added external name generator library

---
+++ src/app/libs/random-name/index.js
@@ -0,0 +1,33 @@
+var names = require('./names.ts');
+var first = require('./first-names.ts');
+var middle = require('./middle-names.json');
+var place = require('./places.json');
+
+function r(names) {
+    return function () {
+        return names[~~(Math.random() * names.length)];
+    };
+}
+
+var random = (module.exports = function () {
+    return random.first() + ' ' + random.last();
+});
+
+random.first = r(first);
+random.last = r(names);
+random.middle = r(middle);
+random.place = r(place);
+
+if (!module.parent) {
+    var l = process.argv[2] || 10;
+    while (l--)
+        console.log(
+            random.first(),
+            '.',
+            random.middle(),
+            '.',
+            random.last(),
+            ',',
+            random.place()
+        );
+}
